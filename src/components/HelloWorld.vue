5baece1b593fecc1cd20b5bc63d8a62a11fd42c9
xudunyao <xudunyao@126.com>
2023-11-12 19:03:17 +0800

    fix: dev提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to test HMRdddddddddd
     </p>
   </div>
 
@@ -23,7 +23,7 @@ const count = ref(0)
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
-    >, the official Vue + Vite starter
+    >, the official Vue + Vite startr
   </p>
   <p>
     Install
